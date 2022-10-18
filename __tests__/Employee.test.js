12ea66e4a1f13c97da589fadf9587cadf5af8077
Bettina Staartjes <bbstaars@gmail.com>
2022-10-18 17:17:06 -0400

    added menu choices and prompt for adding an engineer

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,20 @@
 const Employee = require('../lib/Employee.js');
+const employee = new Employee ('Bettina', '123456', 'bettina@gmai.com');
 
-test('creates a health enemy object', () => {
-    const employee = new Employee('health');
-  
-    expect(employee.name).toBe('health');
-    expect(employee.value).toEqual(expect.any(Number));
-  });
+test('testing if we can get the constructor values for the employee object', () => {
+  expect(employee.name).toBe('Bettina');
+  expect(employee.id).toBe('123456');
+  expect(employee.email).toBe('bettina@gmai.com');
+});
 
-  test('creates a random employee object', () => {
-    const employee = new Employee();
-  
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.name.length).toBeGreaterThan(0);
-    expect(employee.value).toEqual(expect.any(Number));
-  });
+test ('test if we can get the name from the getName() method', () => {
+  expect(employee.getName()).toBe('Bettina');
+});
+
+test ('test if we can get the ID from mthe getId() method', () => {
+  expect(employee.getId()).toBe('123456');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+  expect(employee.getEmail()).toBe('bettina@gmai.com');
+});
--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-//this is where we're keeping track of our team and storing all of our info... 
-const team = [
-]
-
 //this is how we import the Employee, Manager class, etc.
-const Employee = require ("./lib/Employee")
 const Manager = require ("./lib/Manager")
 const Engineer = require ("./lib/Engineer")
 const Intern = require ("./lib/Intern")
 const inquirer = require ("inquirer")
+//?
+const generateSite = require('./src/page-template');
+//file system required & path
+const fs = require ("fs");
+const path = require("path");
+//??
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "index.html");
 
-//pass three paramaters (name, id, email)
+
+//this is where we're keeping track of our team and storing all of our info... EMPTY ARRAY
+const teamMembers = [];
+
+
+//tutoring notes: pass three paramaters (name, id, email)
     //const employee = new Employee ("Bettina", "2384", "bettina@gmail.com"
     //each thing in quotation marks is a string; this employee variable is a new instance of this employee. try console logging (employee.name) and employee.id and employee.sayhi -- name and id are properties and say hi is a method (acts like a function); if you run the app w that you'll get the above info in the terminal; a class is an object that we pass parameters in.
 
 
+    
     //each question is going to be an object. need the [ to start bc its an array. objects = {}
-const managerQuestions = [
+const promptManager = () => {
+    return inquirer.prompt ([
     {
         type: "input",
         name: "name",
-        message: "Please enter your name (or your managers name)"
+        message: "What is your name?"
     }, 
     {
         type: "input",
         name: "id",
-        message: "What's your ID?"
+        message: "What's your employee ID?"
     }, 
     {
         type: "input",
@@ -36,25 +46,13 @@ const managerQuestions = [
         name: "officeNumber",
         message: "What's your office number?"
     }
-]
-
-const employeeQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Please enter employees name."
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Please enter employees ID."
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter employee's email."
-    }
-]
+]).then(answers => {
+    console.log(answers);
+    const manage = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+    teamMembers.push(Manager);
+    promptMenu();
+})
+};
 
 const menuChoices = {
     type: "list",
@@ -66,37 +64,53 @@ const menuChoices = {
     ]
 }
 
-//define a function called promptManager -- paranthesis, curly braces. 
-function promptManager () {
-    inquirer.prompt (managerQuestions)
-    //any variable you put in there (after the .then) will be whatever this is returning. 
-    .then (managerAnswers => {
-        console.log (managerAnswers)
-        //new Manager is two words bc its a new instance of this manager class; put all 4 things into these parenthesis ();
-        const manager = new Manager (managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber)
-        team.push (manager) //now manager is inside team array at the top
-        //call the prompt menu function for this to pop up after you add manager to team
-        promptMenu()
-    })
-}
 
-function promptEmployee () {
-    inquirer.prompt (employeeQuestions)
-    .then (employeeAnswers => {
-        console.log (employeeAnswers)
-        const employee = new Employee (employeeAnswers.name, employeeAnswers.id, employeeAnswers.email, employeeAnswers.id)
-        team.push (employee)
-        promptMenu()
-    })
-}
 
 //give the user an option of create an employee or finish w team...
-function promptMenu () {
-    inquirer.prompt (menuChoices)
-    .then(menuAnswers => {
-        console.log (menuAnswers)
+const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do next? Select an option below.',
+            choices: ['Add an Engineer', 'Add an Intern', 'Finish Building My Team']
+        }])
+        .then(userChoice => {
+            switch (userChoice.menu) {
+                case "Add an Engineer":
+                    promptEngineer();
+                    break;
+                case "Add an Intern":
+                    promptIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+    };
+
+//prompt the Engineer questions/function
+const promptEngineer = () => {
+    console.log(`
+    ==================
+    Add A New Engineer
+    ==================
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name of Engineer',
+        }
+    ]).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.id, answers.email)
+        teamMembers.push(engineer);
+        promptMenu();
     })
-}
+};
+
 
 //this is how we call the prompt manager function!!
 promptManager ()
